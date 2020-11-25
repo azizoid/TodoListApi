7c7b778d8f8ea313876ee8408e966e3b755803cb
azizoid <shaqhuseynov@gmail.com>
2020-11-26 00:35:13 +0100

    added HttpStatuses

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param, Delete, HttpCode, HttpStatus, Header } from '@nestjs/common';
 import { TodosService } from './todos.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
@@ -8,6 +8,7 @@ export class TodosController {
   constructor(private readonly todosService: TodosService) { }
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   create(
     @Body() createTodoDto: CreateTodoDto
   ) {
@@ -16,22 +17,27 @@ export class TodosController {
   }
 
   @Get()
+  @HttpCode(HttpStatus.OK)
+  @Header('Cache-Control', 'none')
   findAll() {
     console.log("findAll")
     return this.todosService.findAll();
   }
 
   @Get(':id')
+  @HttpCode(HttpStatus.OK)
   findOne(@Param('id') id: string) {
     return this.todosService.findOne(id);
   }
 
   @Put(':id')
+  @HttpCode(HttpStatus.OK)
   update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
     return this.todosService.update(id, updateTodoDto);
   }
 
   @Delete(':id')
+  @HttpCode(HttpStatus.OK)
   remove(@Param('id') id: string) {
     return this.todosService.remove(id);
   }
